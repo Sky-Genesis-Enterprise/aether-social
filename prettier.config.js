9faea86965fe7887bea06b51ef258fb820b9f924
liamvnastoria <liam.dispa@skygenesisenterprise.com>
2025-04-17 14:58:11 +0200

    Add file environment config

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+// filepath: /home/liamforsky/Bureau/office/aether-social/prettier.config.js
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+};
